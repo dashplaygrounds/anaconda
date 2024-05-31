9a78d487898064100e8d5d033ee7a68d0314bade
markjulius-silverio_SYNPULSE <markjulius.silverio@synpulse.com>
2024-05-31 13:56:13 +0800

    feat: docker anaconda

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+mkdir -p ~/Documents/Refactored_Py_DS_ML_Bootcamp-master
+ln -s ~/Documents/Refactored_Py_DS_ML_Bootcamp-master ./notebooks
 docker network create spnet
 docker compose up -d
+docker logs -f anaconda
